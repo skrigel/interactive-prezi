26699266506e5cf30c3c6bfd60aea7f13877a361
skrigel <skrigel@mit.edu>
2025-02-17 10:06:59 -0500

    update vite config and package.json with correct page links

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://skrigel.github.io",
+  base: "/interactive-prezi/",
   build: {
     outDir: "dist",
   },
